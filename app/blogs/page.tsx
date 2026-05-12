f58f057f8f2b953f22abe938cb5f3434604957a6
niloufar_alinezhad <niloufar_alinezhad@fdeshragh.com>
2026-05-12 19:37:05 +0330

    module css and clsx styles

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,6 +1,7 @@
 import BlogList from "@/component/BlogList";
 import Skeleton from "@/component/skeleton";
 import { Suspense } from "react";
+import blog from '@/component/ui/blog.module.css'
 
 export const revalidate = 60;
 
@@ -8,7 +9,7 @@ export default async function BlogsPage() {
     return (
         <div className="flex min-h-screen flex-col p-6 bg-gradient-to-b from-slate-50 to-slate-100">
             <h1 className="text-3xl font-bold mb-4">Blogs</h1>
-            <p>The blog page with its features.</p>
+            <p className={blog.shape}>The blog page with its features.</p>
             <div className="mt-6">
                 <Suspense fallback={<Skeleton className="m-5" lines={5} />}>
                     <BlogList />
